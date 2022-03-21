4ef3ff61c96a000aa3bf68766e26c4e5799b958a
juancedeno96 <cedenojj7@gmail.com>
2022-03-21 12:26:28 -0600

    added routes

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const crypto = require("crypto");
-
+const cookieParser = require("cookie-parser");
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-
+const jwt = require("jsonwebtoken");
 const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN;
 const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN;
+let refreshToken = [];
 
 const smsKey = process.env.SMS_SECRET_KEY;
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser());
+
 app.post("/sendOTP", (req, res) => {
   const phone = req.body.phone;
   const otp = Math.floor(100000 + Math.random() * 900000);
@@ -51,10 +53,86 @@ app.post("/verifyOTP", (req, res) => {
     .digest("hex");
 
   if (newCalculatedHash === hashValue) {
-    return res.status(202).send({ msg: `user confirmed` });
+    refreshToken.push(refreshToken);
+    const accessToken = jwt.sign({ data: phone }, JWT_AUTH_TOKEN, {
+      expiresIn: "30s",
+    });
+    const refreshToken = jwt.sign({ data: phone }, JWT_REFRESH_TOKEN, {
+      expiresIn: "30s",
+    });
+
+    res
+      .status(202)
+      .cookie("accessToken", accessToken, {
+        expires: new Date(new Date().getTime() + 30 * 1000),
+        sameSite: "strict",
+        httpOnly: true,
+      })
+      .cookie("authSession", true, {
+        expires: new Date(new Date().getTime() + 30 * 1000),
+      })
+      .cookie("refreshToken", refreshToken, {
+        expires: new Date(new Date().getTime() + 3557600000),
+        sameSite: "strict",
+        httpOnly: true,
+      })
+      .cookie("refreshTokenID", true, {
+        expires: new Date(new Date().getTime() + 3557600000),
+      })
+      .send({ msg: `Device Verified` });
   } else {
     return res.status(400).send({ verification: false, msg: `Incorrect OTP` });
   }
+});
+
+async function authenticateUser(req, res, next) {
+  const accessToken = req.cookies.accessToken;
+
+  jwt.verify(accessToken, JWT_AUTH_TOKEN, async (err, phone) => {
+    if (phone) {
+      req.phone = phone;
+      next();
+    } else if (err.message === "TokenExpiredError") {
+      return res
+        .status(403)
+        .send({ success: false, msg: `Access Token Expired` });
+    } else {
+      console.error(err);
+      res.status(403).send({ err, msg: `user not authenticated` });
+    }
+  });
+}
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken)
+    return res
+      .status(403)
+      .send({ msg: `refresh Token not found, Please login again` });
+  if (!refreshTokens.includes(refreshToken))
+    return res.status(403).send({ msg: `Refresh Token Blocked, login again` });
+  jwt.verify(refreshToken, JWT_REFRESH_TOKEN, (err, phone) => {
+    if (!err) {
+      const accessToken = jwt.sign({ data: phone }, JWT_AUTH_TOKEN, {
+        expiresIn: "30s",
+      });
+      res
+        .status(202)
+        .cookie("accessToken", accessToken, {
+          expires: new Date(new Date().getTime() + 30 * 1000),
+          sameSite: "strict",
+          httpOnly: true,
+        })
+        .cookie("authSession", true, {
+          expires: new Date(new Date().getTime() + 30 * 1000),
+        })
+        .send({ previousSessionExpiry: true, success: true });
+    } else {
+      return res
+        .status(403)
+        .send({ success: false, msg: `Invalid Refresh Token` });
+    }
+  });
 });
 
 app.listen(4000);
