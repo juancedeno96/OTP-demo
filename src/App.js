3d970a4f45b76e0c5d1fc448e15d8c5227f4eef0
juancedeno96 <cedenojj7@gmail.com>
2022-03-23 11:39:31 -0600

    imported components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import StepForm from './components/StepForm';
-import "./App.css";
+import React from 'react';
+import StepForm from './components/StepForm'
+import auth from './auth';
+import Home from './components/home'
 
 function App() {
-  return <div className="App">
-    <StepForm/>
-  </div>;
+	if (auth.isAuthenticated()) return <Home />;
+	else return <StepForm />;
 }
 
 export default App;
